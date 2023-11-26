7eea3f0fcea6f9e0425d51578e5836e6983b96ab
yaginumatti <ryohei122yagi@gmail.com>
2023-11-26 13:40:57 +0900

    this.jsの修正

--- step4/this/this.js
+++ step4/this/this.js
@@ -1,3 +1,4 @@
+console.log('>オブジェクトに関数を設定した場合におけるthisのサンプル');
 const obj1 = {
   name: 'これはobj1です',
   test: function() {
@@ -10,4 +11,24 @@ console.log(obj1); // => {name: "これはobj1です", test: f}
 
 obj1.test();
 // => {name: "これはobj1です", test: f}
+// => true
+
+console.log('クラスにおけるthisのサンプル');
+class MyClass {
+  constructor() {
+    this.name = 'これはMyClassです';
+  }
+
+  test() {
+    console.log(this);
+    console.log(this === instance1);
+  }
+}
+
+const instance1 = new MyClass();
+
+console.log(instance1); // => MyClass { name: "これはMyClassです"}
+
+instance1.test();
+// => MyClass { name: "これはMyClassです"}
 // => true
